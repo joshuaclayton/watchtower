039d7d87b0f9c985c0acf906af8b371cf0e57a33
Joshua Clayton <jclayton@fusionary.com>
2009-04-24 20:38:44 -0400

    Added info to readme and wrote class method forcing public handling of exceptions

--- lib/watchtower/application_controller_base.rb
+++ lib/watchtower/application_controller_base.rb
@@ -12,7 +12,29 @@ module Watchtower
         
         alias_method_chain :rescue_action_in_public, :watchtower
       end
+      
+      base.extend ClassMethods
     end
+    
+    module ClassMethods
+      def force_public_request_handling!
+        
+        self.class_eval do
+          def consider_all_requests_local_with_override(*args)
+            false
+          end
+          
+          def local_request_with_override?(*args)
+            false
+          end
+          
+          alias_method_chain :consider_all_requests_local, :override
+          alias_method_chain :local_request?, :override
+        end
+        
+      end
+    end
+    
   end
 end
   
